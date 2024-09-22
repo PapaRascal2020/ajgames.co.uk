377bfcecb3569ced8b2a425c3a141745780cd42c
Ashley Johnson <61059402+PapaRascal2020@users.noreply.github.com>
2024-09-22 17:04:25 +0100

    Fixed API.php after migrating to DO

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+use App\Http\Controllers\PlayerController;
+
+Route::post('login', [PlayerController::class, 'login']);
+Route::post('add-friend', [PlayerController::class, 'addFriend']);
+Route::get('online-status/{playerId}', [PlayerController::class, 'getOnlineStatus']);
+Route::get('photon-room/{playerId}', [PlayerController::class, 'getPhotonRoom']);
+Route::post('update-status-room', [PlayerController::class, 'updateStatusAndRoom']);
+Route::post('register', [PlayerController::class, 'register']);
+Route::get('/player/{playerId}/friends', [PlayerController::class, 'listFriends']);
